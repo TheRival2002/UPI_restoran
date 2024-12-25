f61c48fd4fe573f80be46de5b10a649a9ed4b0a2
Šime Jadrijev <Sime.Jadrijev@infobip.com>
2024-12-25 23:44:43 +0100

    implement validation and service layer

--- backend/src/services/MealsService.ts
+++ backend/src/services/MealsService.ts
@@ -7,4 +7,26 @@ export class MealsService {
     public async findAll(): Promise<Meal[]> {
         return this.mealsRepository.findAll();
     }
+
+    public async createMeal(meal: Meal) {
+        if (!this.mealIsValid(meal))
+            return;
+
+        return this.mealsRepository.createMeal(meal);
+    }
+
+    private async mealIsValid(meal: Meal) {
+        const mealName = meal.name.trim();
+        const mealNameIsUnique = await this.mealsRepository.mealNameIsUnique(mealName);
+
+        if (!mealNameIsUnique)
+            throw new Error('Meal already exists!');
+
+        if (meal.price < 0)
+            throw new Error('Price can\'t be negative!');
+
+        return true;
+    }
+
+
 }
