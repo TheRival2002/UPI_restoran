d1b17bc297dc1b1e828ac7d3d028acf0757fe258
Marin Čović <marincovic4@gmail.com>
2024-12-30 11:48:36 +0100

    added validation, refactored code

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { User } from '../entities/User';
 import { AuthService } from '../services/AuthService';
 import { LoginCredentials } from '../types/auth';
@@ -17,18 +17,18 @@ export class AuthController {
         this.authRouter.post('/login', this.login.bind(this));
     }
 
-    private async register(req: Request, res: Response) {
+    private async register(req: Request, res: Response, next: NextFunction) {
         try {
             const userData: User = req.body;
             const user = await this.authService.register(userData);
 
             res.status(201).json({ message: 'User created', user });
         } catch (error) {
-            res.status(500).json({error: (error as Error).message});
+            next(error);
         }
     }
 
-    private async login(req: Request, res: Response) {
+    private async login(req: Request, res: Response, next: NextFunction) {
         try {
             const userAuthData: LoginCredentials = req.body;
             const { accessToken, refreshToken } = await this.authService.login(userAuthData);
@@ -36,7 +36,7 @@ export class AuthController {
             res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // maxAge is in milliseconds, so it is 1 day
             res.status(200).json({ accessToken });
         } catch (error) {
-            res.status(500).json({ error: (error as Error).message });
+            next(error);
         }
     }
 }
