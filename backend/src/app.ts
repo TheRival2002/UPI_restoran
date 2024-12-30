bc40c3b60c35e5eb211c50b32e9480f5131e85bf
Marin Čović <marincovic4@gmail.com>
2024-12-30 12:56:57 +0100

    configured cors, set up auth middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,11 @@ import { MainRouter } from './routes/MainRouter';
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
+const corsOptions = {
+    origin: process.env.BASE_URL,
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}
 const cors = require('cors');
 
 const PORT = process.env.API_PORT || 3000;
@@ -15,7 +20,7 @@ const app: Express = express();
 const router = new MainRouter();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/api', router.routes);
 app.use(errorHandler);
 
--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -7,18 +7,17 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
 
-    if (!authHeader) {
-        return res.status(401);
-    }
+    if (!authHeader)
+        return res.sendStatus(401);
 
-    const token = authHeader.split(' ')[1];
+    const token = authHeader?.split(' ')[1];
 
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err: Error, decoded: { id: string }) => {
             if (err) {
-                return res.status(403);
+                return res.sendStatus(403);
             }
 
             req.user = decoded.id;
