e8e5dd67362db3c743654ec8fe37a9751aee943b
Šime Jadrijev <Sime.Jadrijev@infobip.com>
2025-01-08 00:02:57 +0100

    implement InputField component

---
+++ frontend/src/components/InputField/InputField.tsx
@@ -0,0 +1,31 @@
+import c from './../../styles/login.module.css';
+import { ChangeEventHandler } from 'react';
+
+interface InputFieldProps {
+    label: string;
+    type: string;
+    inputId: string;
+    placeholder: string;
+    value: string;
+    onChange: ChangeEventHandler<HTMLInputElement>;
+    required: boolean;
+}
+
+export default function InputField({
+    label, type, inputId, placeholder, value, onChange, required
+}: InputFieldProps) {
+    return (
+        <div className={c.inputGroup}>
+            <label htmlFor={inputId}>{label}</label>
+            <input
+                type={type}
+                id={inputId}
+                name={inputId}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                required={required}
+            />
+        </div>
+    );
+}
