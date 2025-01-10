2c73d2262de4fd74e3a0b044c83c05d7e58fe465
Gadza03 <mgadza@pmfst.hr>
2025-01-10 19:21:02 +0100

    Started with landing page

---
+++ frontend/src/pages/landing/Navbar.tsx
@@ -0,0 +1,6 @@
+import Logo from '../../assets/images/Logo.svg';
+export default function Navbar(){
+    return(
+        <img src={Logo} alt="Logo" />
+    );
+}
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,7 +1,8 @@
 import { useRoutes } from 'react-router';
 import LoginPage from './pages/auth/LoginPage.tsx';
 import RegisterPage from './pages/auth/RegisterPage.tsx';
-import c from './styles/homePage.module.css';
+import LandingPage from './pages/landing/LandingPage.tsx';
+//import c from './styles/homePage.module.css';
 
 // ----------------------------------------------------------------------
 
@@ -22,26 +23,27 @@ export default function Router() {
         },
         {
             path: '/',
-            element: ( // TODO ovo cemo izmijenit, imat cemo drugu komponentu za home page
-                <div className={c.welcomeContainer}>
-                    <h1 className={c.welcomeHeader}>Welcome to FoodHub!</h1>
-                    <p className={c.welcomeSubtitle}>
-                        Your favourite foods delivered fast at your door.
-                    </p>
-                    <button
-                        onClick={() =>
-                            (window.location.href = '/auth/register')
-                        }
-                    >
-                        Register
-                    </button>
-                    <button
-                        onClick={() => (window.location.href = '/auth/login')}
-                    >
-                        Login
-                    </button>
-                </div>
-            ),
+            element:(<LandingPage/>)
+            // element: ( // TODO ovo cemo izmijenit, imat cemo drugu komponentu za home page
+            //     <div className={c.welcomeContainer}>
+            //         <h1 className={c.welcomeHeader}>Welcome to FoodHub!</h1>
+            //         <p className={c.welcomeSubtitle}>
+            //             Your favourite foods delivered fast at your door.
+            //         </p>
+            //         <button
+            //             onClick={() =>
+            //                 (window.location.href = '/auth/register')
+            //             }
+            //         >
+            //             Register
+            //         </button>
+            //         <button
+            //             onClick={() => (window.location.href = '/auth/login')}
+            //         >
+            //             Login
+            //         </button>
+            //     </div>
+            // ),
         },
     ]);
 }
