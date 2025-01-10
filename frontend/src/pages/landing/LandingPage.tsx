324eab5716ae4befe9bcb8c23838621efe14038b
Gadza03 <mgadza@pmfst.hr>
2025-01-11 00:41:28 +0100

    About us section done

--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -3,9 +3,12 @@ import c from '@styles/landingPage.module.css';
 import Section from './Section';
 import HomeBanner from '@assets/images/landingPageImgs/home-banner-background.png';
 import BannerFood from '@assets/images/landingPageImgs/home-banner-image.png';
+import AboutFoodImg from '@assets/images/landingPageImgs/about-background-image.png';
+import AboutBanner from '@assets/images/landingPageImgs/about-background.png';
 export default function LandingPage(){
     return(
         <div className={c.landingPage}>
+            
             <div className={c.homeBannerImg}>
                 <img src={HomeBanner} alt="banner" />
             </div>  
@@ -20,6 +23,20 @@ export default function LandingPage(){
                 buttonText="Order Now"
                 imgUrl={BannerFood}
             />
+
+            <Section      
+                title='ABOUT US'           
+                h3="Food Is An Important Part Of A Balanced Diet"
+                p="Lorem ipsum dolor sit amet consectetur. Non tincidunt 
+                    magna non et elit. Dolor  turpis molestie dui
+                    magnis facilisis at fringilla quam."
+                buttonText="Learn More"
+                imgUrl={AboutFoodImg}
+            >             
+                <div className={c.aboutDecoration}>
+                    <img src={AboutBanner} alt="aboutbanner" />
+                </div>
+            </Section>
 
         </div>
     );
--- frontend/src/pages/landing/Section.tsx
+++ frontend/src/pages/landing/Section.tsx
@@ -1,32 +1,13 @@
-// import c from './../../styles/inputField.module.css';
-// import { ChangeEventHandler } from 'react';
-
-// interface InputFieldProps {
-//     label: string;
-//     inputId: string;
-//     placeholder: string;
-//     value: string;
-//     onChange: ChangeEventHandler<HTMLInputElement>;
-//     type?: string;
-//     error?: string;
-// }
 import c from '@styles/landingPage.module.css';
 
-// export default function InputField({
-//     label,
-//     inputId,
-//     placeholder,
-//     value,
-//     onChange,
-//     type = 'text',
-//     error,
-// }: InputFieldProps) {
 interface SectionProps {
     title?: string;
     h3: string;
     p: string;
     buttonText: string;
     imgUrl?:string;
+    decoration?:string;
+    children?:React.ReactNode;
 }
 
 export default function Section({
@@ -35,6 +16,7 @@ export default function Section({
     p,        
     buttonText,
     imgUrl,
+    children,
 }: SectionProps) {
     return (
         <div className='container'>
@@ -44,6 +26,7 @@ export default function Section({
                 <p>{p}</p>
                 <button>{buttonText}</button>
                 {imgUrl && <img className={c.sectionImg} src={imgUrl} alt="photo" />}
+                {children}
             </div>  
         </div>
     );
