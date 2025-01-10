25bd0b0757a9573d2dca575700b0392fc209d61d
Gadza03 <mgadza@pmfst.hr>
2025-01-10 21:36:44 +0100

    Navbar with mui

--- frontend/src/pages/landing/Navbar.tsx
+++ frontend/src/pages/landing/Navbar.tsx
@@ -1,6 +1,62 @@
-import Logo from '../../assets/images/Logo.svg';
+import Logo from '@assets/images/landingPageImgs/Logo.svg';
+import HomeIcon from '@mui/icons-material/Home';
+import ContactIcon from '@mui/icons-material/Phone';
+import AboutIcon from '@mui/icons-material/Info';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Drawer } from '@mui/material';
+import { useState } from 'react';
+import c from '@styles/landingPage.module.css';
+
 export default function Navbar(){
+    const [ openMenu, setOpenMenu ] = useState(false);
+    const menuOptions = [
+        {
+            text: 'Home',            
+            icon: <HomeIcon sx={{ color: 'var(--primary-orange)' }}/>
+        },
+        {
+            text: 'About',            
+            icon: <AboutIcon sx={{ color: 'var(--primary-orange)' }}/>
+        },
+        {
+            text: 'Contact',            
+            icon: <ContactIcon sx={{ color: 'var(--primary-orange)' }}/>
+        }
+    ];
     return(
-        <img src={Logo} alt="Logo" />
+        <div className="container">
+            <nav>
+                <div className={'nav-logo'}>
+                    <img src={Logo} alt="logo" />
+                </div>
+                
+                <div className="navbar-menu">
+                    <MenuIcon onClick={() => setOpenMenu(true)} />
+                </div>
+                <Drawer open={openMenu} onClose={() => setOpenMenu(false)}
+                    anchor='right'>
+                    <Box sx={{ width: 250 }}
+                        role = "presentation"
+                        onClick = {() => setOpenMenu(false)}                   
+                    >  
+                        <List>
+                            {menuOptions.map((item) => (
+                                <ListItem key={item.text}>
+                                    <ListItemButton>
+                                        <ListItemIcon>{item.icon}</ListItemIcon>
+                                        <ListItemText primary={item.text}/>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                            <div className={c.signInWrapper}>
+                                <button onClick={() => (window.location.href = '/auth/login')}>Sign In</button>
+                            </div>  
+                        </List>                                          
+                    </Box>
+                </Drawer>
+
+            </nav>
+        </div>   
+        
     );
 }
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -24,26 +24,7 @@ export default function Router() {
         {
             path: '/',
             element:(<LandingPage/>)
-            // element: ( // TODO ovo cemo izmijenit, imat cemo drugu komponentu za home page
-            //     <div className={c.welcomeContainer}>
-            //         <h1 className={c.welcomeHeader}>Welcome to FoodHub!</h1>
-            //         <p className={c.welcomeSubtitle}>
-            //             Your favourite foods delivered fast at your door.
-            //         </p>
-            //         <button
-            //             onClick={() =>
-            //                 (window.location.href = '/auth/register')
-            //             }
-            //         >
-            //             Register
-            //         </button>
-            //         <button
-            //             onClick={() => (window.location.href = '/auth/login')}
-            //         >
-            //             Login
-            //         </button>
-            //     </div>
-            // ),
+            
         },
         {
             element: <CompactLayout />,
