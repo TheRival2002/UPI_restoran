b6a6e101665591164330593231f939d896c2d0c1
Šime Jadrijev <Sime.Jadrijev@infobip.com>
2025-01-17 00:44:27 +0100

    implement userContext and useAuthContext custom hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router';
 import Router from './router.tsx';
+import { UserProvider } from './providers/UserContext.tsx';
 
 // ----------------------------------------------------------------------
 
 const App = () => {
     return (
-        <HelmetProvider>
-            <BrowserRouter>
-                <Router/>
-            </BrowserRouter>
-        </HelmetProvider>
+        <UserProvider>
+            <HelmetProvider>
+                <BrowserRouter>
+                    <Router/>
+                </BrowserRouter>
+            </HelmetProvider>
+        </UserProvider>
     );
 };
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { UserProvider } from './providers/UserContext.tsx';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <StrictMode>
-        <UserProvider>
-            <App/>
-        </UserProvider>
+        <App/>
     </StrictMode>
 );
