b64fd85c62d01316da255faae26cd057226b912d
TheRival2002 <marincovic4@gmail.com>
2025-01-27 22:51:18 +0100

    add edit user page navigation

---
+++ frontend/src/pages/auth/EditUserPage.tsx
@@ -0,0 +1,11 @@
+import { Helmet } from 'react-helmet-async';
+
+// ----------------------------------------------------------------------
+
+export default function EditUserPage() {
+    return (
+        <Helmet>
+            <title> Edit profile </title>
+        </Helmet>
+    );
+}
--- frontend/src/routes/elements.tsx
+++ frontend/src/routes/elements.tsx
@@ -21,6 +21,7 @@ export const LandingPage = Loadable(lazy(() => import('@pages/landing/LandingPag
 export const AllMealsPage = Loadable(lazy(() => import('@pages/meals/AllMealsPage.tsx')));
 export const SingleMealPage = Loadable(lazy(() => import('@pages/meals/SingleMealPage.tsx')));
 export const DailyOffers = Loadable(lazy(() => import('@pages/daily/DailyOffersPage')));
+export const EditUserPage = Loadable(lazy(() => import('@pages/auth/EditUserPage.tsx')));
 
 // ERROR
 export const Page403 = Loadable(lazy(() => import('@pages/error/Page403.tsx')));
